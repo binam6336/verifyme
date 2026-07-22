9e948b9589b6ffc47d31dfa82a9ce31000a57679
Mahyarsalehee <mhyarsalhy9@gmal.com>
2026-07-22 14:43:04 -0700

    first database sql configruation

---
+++ w4/Project/config/database-sql/DB.sql
@@ -0,0 +1,3 @@
+-- Comany database 
+    CREATE DATABASE company CHARACTER SET utf8 COLLATE utf8_general_ci;
+    
---
+++ w4/Project/config/database-sql/companyTables.sql
@@ -0,0 +1,49 @@
+-- create tables
+        -- user
+            CREATE TABLE user (
+            id int PRIMARY KEY,
+            lastName varchar(255),
+            firstName varchar(255),
+            companyname varchar(255),
+            email varchar(255),
+            username varchar(255),
+            phone varchar(255),
+            password varchar(255)
+            );
+            -- insert test
+            INSERT INTO user ( firstName , lastName , companyname , email , username , phone , password)
+            VALUES ('test' , 'test', 'test','test','test','test','test');
+
+        -- ptroduct
+            CREATE TABLE product (
+            id int PRIMARY KEY,
+            name varchar(255),
+            category varchar(255),
+            count varchar(255),
+            userid varchar(255)
+            );  
+            -- product test
+            INSERT INTO product ( name , category , count , userid )
+            VALUES ('test' , 'test', 'test','test');
+
+        -- licences
+            CREATE TABLE licences (
+            id int PRIMARY KEY,
+            licenceshash varchar(255),
+            expire varchar(255),
+            productid varchar(255)
+            );  
+            -- licences test
+            INSERT INTO licences ( licenceshash , expire , productid )
+            VALUES ('test' , 'test', 'test');
+
+        -- authorization
+            CREATE TABLE authorization (
+            id int PRIMARY KEY,
+            token varchar(255),
+            expire varchar(255),
+            ip varchar(255)
+            );  
+            -- authorization test
+            INSERT INTO authorization ( token , expire , ip )
+            VALUES ('test' , 'test', 'test');
