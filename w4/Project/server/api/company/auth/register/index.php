7ce6c1565c561263a52e56af56140c6f499dbba7
Mahyarsalehee <mhyarsalhy9@gmal.com>
2026-07-23 16:24:28 -0700

    add regster mock

--- w4/Project/server/api/company/auth/register/index.php
+++ w4/Project/server/api/company/auth/register/index.php
@@ -1,13 +1,27 @@
 <?php
+// حل مشکل دسترسی و پیش‌پرواز مرورگر
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
 
-if($_SERVER['REQUEST_METHOD'] == ' POST'){
+if($_SERVER['REQUEST_METHOD'] == "POST"){
     $response = [
   "status"=> "error",
-  "message"=> "ثبت‌نام با موفقیت انجام شد222.",
+  "message"=> "ثبت‌نام با موفقیت انجام شد55S22.",
     "errors" => [
         "mobile" => "حطا در پردازش"
     ]
     ];
 
-    echo json_encode($response);
-    }
+  }
+  else{
+    $response = [
+  "status"=> "error",
+  "message"=> "خطا در انجام عملیات",
+    "errors" => [
+        "method" => "متد ارسالی صحیح نیست"
+    ]
+    ];
+  }
+echo json_encode($response);
