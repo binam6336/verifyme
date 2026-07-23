df33dde62c57d905670d1b4aac9b744541465f8e
Mahyarsalehee <mhyarsalhy9@gmal.com>
2026-07-23 15:33:46 -0700

    add Auth and User manager model and Conroller

---
+++ w4/Project/server/Src/App/Model/Usermanager.php
@@ -0,0 +1,11 @@
+<?php
+//
+// @description @methodes
+// ** this file is connecting to Database for Register ( insert new user in DB ) and Get User data
+// ** user manager model methodes :
+// ** NewUser
+// ** GetUser
+// ** GetAllUsers
+// ** UpdateUser
+// ** DeleteUser
+//
