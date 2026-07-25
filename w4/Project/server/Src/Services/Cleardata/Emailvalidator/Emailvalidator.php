4441df0496254cdac9af9fa3c1e52b97eab342a2
Mahyarsalehee <mhyarsalhy9@gmal.com>
2026-07-25 11:48:32 -0700

    add mobile and email validator demo

---
+++ w4/Project/server/Src/Services/Cleardata/Emailvalidator/Emailvalidator.php
@@ -0,0 +1,17 @@
+<?php
+
+// mock and demo of Emailvalidator
+
+namespace Services\Cleardata\Emailvalidator;
+
+class Emailvalidator
+{
+
+    public function ClearData(array $data)
+    {
+        foreach ($data as $key => $value) {
+            $value = $value;
+        }
+        return $data;
+    }
+}
---
+++ w4/Project/server/Src/Services/Cleardata/Mobilevalidator/Mobilevalidator.php
@@ -0,0 +1,17 @@
+<?php
+
+// mock and demo of Mobilevalidator
+
+namespace Services\Cleardata\Mobilevalidator;
+
+class Mobilevalidator
+{
+
+    public function ClearData(array $data)
+    {
+        foreach ($data as $key => $value) {
+            $value = $value;
+        }
+        return $data;
+    }
+}
