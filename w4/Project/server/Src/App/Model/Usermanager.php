acb43746bb70f49a369f330da7e35b36ec13cf3e
Mahyar <mahyar@example.com>
2026-08-22 12:56:40 +0330

    update usermanager and profile img

--- w4/Project/server/Src/App/Model/Usermanager.php
+++ w4/Project/server/Src/App/Model/Usermanager.php
@@ -68,8 +68,9 @@ class Usermanager
 
 
         if ($stmt->execute()) {
-            $token = $this->auth->CreateToken($this->conn->lastInsertId())['token'];
             $userid = $this->conn->lastInsertId();
+
+            $token = $this->auth->CreateToken($userid)['token'];
 
             return [
                 "status" => true,
@@ -86,76 +87,89 @@ class Usermanager
     public function GetUser(array $data)
     {
 
-        // change token to user id
-        $token = $data['token'];
+        if (count($data) != 1 || !isset($data['token']) && !isset($data['phone'])) {
 
-        $userid = $this->auth->GetIdWithToken($token)[0] ?? false;
+            // log error
+            $this->logger->warning("در متد دریافت کاربر دیتا ورودی اشتباهه یا ناقصه یا کانفلیکت داره ؛ دیتای ورودی : ", [$data]);
 
-        if ($userid == false) {
             return [
                 "status" => false
             ];
         }
 
-        // $this->logger->info("recived userid / get user", [
-        //     $userid
-        // ]);
+        if (isset($data['token'])) {
+            // change token to user id
+            $token = $data['token'];
 
-        // sql
-        $sql = "
-        SELECT * FROM
-        user
-        WHERE id = :id
-        ";
+            $userid = $this->auth->GetIdWithToken($token)[0] ?? false;
 
-        $stmt = $this->conn->prepare($sql);
-
-        $stmt->bindParam(":id", $userid);
-
-        if ($stmt->execute()) {
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            $this->logger->info("fetch result / get user", [
-                $result
-            ]);
-
-            if ($result == null || empty($result)) {
+            if ($userid == false) {
                 return [
                     "status" => false
                 ];
             }
 
-            $firstname = $result[0]["firstName"] ?? "NULL";
-            $lastName = $result[0]["lastName"] ?? "NULL";
-            $companyname = $result[0]["companyname"] ?? "NULL";
-            $email = $result[0]["email"] ?? "NULL";
-            $username = $result[0]["username"] ?? "NULL";
-            $phone = $result[0]["phone"] ?? "NULL";
-            $company_status = $result[0]["company_status"] ?? "NULL";
-            $id =  $result[0]["id"] ?? "NULL";
+            // $this->logger->info("recived userid / get user", [
+            //     $userid
+            // ]);
 
-            $userdata = [
-                "status" => true,
-                "firstname" => $firstname,
-                "lastName" => $lastName,
-                "companyname" => $companyname,
-                "email" => $email,
-                "username" => $username,
-                "phone" => $phone,
-                "company_status" => $company_status,
-                "id" => $id
+            // sql
+            $sql = "
+        SELECT * FROM
+        user
+        WHERE id = :id
+        ";
 
-            ];
+            $stmt = $this->conn->prepare($sql);
 
-            $this->logger->info(" user data to return / get user", [
-                $userdata
-            ]);
+            $stmt->bindParam(":id", $userid);
 
-            return $userdata;
-        } else {
-            return [
-                "status" => false
-            ];
+            if ($stmt->execute()) {
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                $this->logger->info("fetch result / get user", [
+                    $result
+                ]);
+
+                if ($result == null || empty($result)) {
+                    return [
+                        "status" => false
+                    ];
+                }
+
+                $firstname = $result[0]["firstName"] ?? "NULL";
+                $lastName = $result[0]["lastName"] ?? "NULL";
+                $companyname = $result[0]["companyname"] ?? "NULL";
+                $email = $result[0]["email"] ?? "NULL";
+                $username = $result[0]["username"] ?? "NULL";
+                $phone = $result[0]["phone"] ?? "NULL";
+                $company_status = $result[0]["company_status"] ?? "NULL";
+                $id =  $result[0]["id"] ?? "NULL";
+
+                $userdata = [
+                    "status" => true,
+                    "firstname" => $firstname,
+                    "lastName" => $lastName,
+                    "companyname" => $companyname,
+                    "email" => $email,
+                    "username" => $username,
+                    "phone" => $phone,
+                    "company_status" => $company_status,
+                    "id" => $id
+
+                ];
+
+                $this->logger->info(" user data to return / get user", [
+                    $userdata
+                ]);
+
+                return $userdata;
+            } else {
+                return [
+                    "status" => false
+                ];
+            }
+        } elseif (isset($data['phone'])) {
         }
     }
     public function GetAllUsers() {}
